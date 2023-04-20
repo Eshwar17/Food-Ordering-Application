6a5d0792b10706feb016fbf5c00fcba8ab4b772a
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-20 11:16:09 +0530

    Created assets folder and added meals.jpeg

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+        <header>
+            <h1>Sindhu Meals</h1>
+            <button></button>
+        </header>
+        <div></div>
+    </React.Fragment>
+  )
+}
+
+export default Header
