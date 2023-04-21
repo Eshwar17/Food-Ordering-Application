071b461b9d83695bb63bb2f689a798b572ee80fc
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-21 12:59:25 +0530

    Created CartIcon.js file

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
   return (
-    <div>
-      Button
-    </div>
+    <button>
+        <span></span>
+        <span></span>
+        <span></span>
+    </button>
   )
 }
 
